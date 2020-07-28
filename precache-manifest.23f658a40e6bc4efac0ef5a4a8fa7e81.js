c3d62fd7b03d5d1c1f7c84a2abfe0765024315c8
Vikas Gupta <vikas.gupti047@gmail.com>
2020-07-28 16:05:13 +0530

    Updates

---
+++ precache-manifest.23f658a40e6bc4efac0ef5a4a8fa7e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de4516d664e3bd7c7b82436a5cd0472",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "58bb0bf7311c2fa9d5ff",
+    "url": "/todo-list/static/css/main.f8758cf5.chunk.css"
+  },
+  {
+    "revision": "9f91c0487c5f3a3248f4",
+    "url": "/todo-list/static/js/2.58f23500.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.58f23500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bb0bf7311c2fa9d5ff",
+    "url": "/todo-list/static/js/main.4163674c.chunk.js"
+  },
+  {
+    "revision": "27e019a4eb32999bd0bf",
+    "url": "/todo-list/static/js/runtime-main.88bd720a.js"
+  }
+]);
